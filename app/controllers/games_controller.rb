677d0bf5cd64d6871aa99aaa1098869541ff09a0
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-08 20:13:55 +0200

    score view implemented with displaying the result that the word ins included or not, but check if english word is missing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,7 +6,22 @@ class GamesController < ApplicationController
       @letters << alphabet[rand(25)]
     end
   end
-  
+
   def score
+    # if
+    word_included = true
+    @letters = params["letters"].downcase.split(" ")
+    @word = params["word"].downcase.split("")
+    @word.each do |char|
+      if @letters.count(char) < @word.count(char)
+        word_included = false
+      end
+    end
+    if word_included
+      @result_message = "The word is included in the letters!"
+    else
+      @result_message = "The word is NOT included in the letters"
+    end 
+    # binding.pry
   end
 end
