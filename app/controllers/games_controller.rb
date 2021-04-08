2977890214ebcd8b5ce75b003cd256277c6b008c
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-08 19:14:00 +0200

    displaying random letters in new-view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,12 @@
 class GamesController < ApplicationController
   def new
+    alphabet = ("A".."Z").to_a
+    @letters = []
+    10.times do
+      @letters << alphabet[rand(25)]
+    end
   end
-
+  
   def score
   end
 end
